90f08e345ce04384552c09ca42b55f61d751bf27
Alfnnnnyy <159158160+Alfnnnnyy@users.noreply.github.com>
2024-02-11 21:19:15 +0700

    Update ConfigRotat.lua

--- ConfigRotat.lua
+++ ConfigRotat.lua
@@ -1,17 +1,17 @@
 ---===== INFO BUYER =====--- 
 InfoLoop = true
-targetinfo = 10 --MENGIRIM WEBHOOK SETIAP BERAPA KALI LOOP?
+targetinfo = 5 --MENGIRIM WEBHOOK SETIAP BERAPA KALI LOOP?
 RingkasWorldName = false --Jadiin False Jika Gamau Nama Worldnya di Ringkas // INI COCOK UNTUK WORLDNYA KEBANYAKAN + KEPANJANGAN AGAR GA BUG WEBHOOK
-MinBW = 700 --APABILA BANWAVE MAKA BOT AKAN OFF
-DurasiOff = 10
 
 ---===== Bot Off =====---
 Istirahat = false --JADIIN FALSE JIKA GAMAU ISTIRAHATIN BOTNYA
 UTC_Time = 9 -- WIB 7 / WITA 8 / WIT 9
 JamTidur = {
-"00:00", "11:45", "17:45", "23:30",
+    "13:20",
+    "14:25",
+    "01:40"
 } -- JAM DAN MENIT BOTNYA OFF (Format 24 Jam)
-DelayTidur = 10 -- BOTNYA OFF SELAMA n DelayTidur (Format Menit)
+DelayTidur = 20 -- BOTNYA OFF SELAMA n DelayTidur (Format Menit)
 
 ---===== New Variable on v4.8 =====---
 TilePNB = 5 --BOTNYA AKAN PNB 3 TILES!! PILIH 1~5
@@ -21,14 +21,15 @@ SisaTree = 1 --BOTNYA AKAN NEXT WORLD APABILA SISA POHON READY BERJUMLAH <= SISA
 
 ---===== BASIC SETTINGS =====---
 target_block = 185 -- TARGET MAX HARVEST (JML BLOCK DI TAS)
-target_seed = 5 -- TARGET MINIMAL SEED UNTUK DITANAM
+target_seed = 10 -- TARGET MINIMAL SEED UNTUK DITANAM
+DelayRecon = 300000 --JEDA RECONNECT (Ms)
 delayht = 160 -- DELAY HARVEST
 delayplant = 180 -- DELAY PLANT
 delaypnb = 190 -- DELAY PNB
 delayplace = 180 -- DELAY PLACE
 delayworld = 35000 -- DELAY MASUK DOOR
-delaydrop = 2000 -- JEDA DROP PER ITEM
-delaytrash = 2000
+delaydrop = 1000 -- JEDA DROP PER ITEM
+delaytrash = 100
 targettrash = 100 -- AUTO TRASH JUNK ITEMS KETIKA MENCUKUPI TARGET
 Turu = true --JADIIN FALSE JIKA GAMAU UNLIMITED WORLD
 
@@ -41,11 +42,6 @@ maxbuy = 2 --MAKSIMAL BOTNYA AKAN REPEAT BELI PACK JIKA
 iditempack = {5706}
 ID_AcuanDrop_Pack = 880 --BOT AKAN DROP PACK DI ACUAN Dirt Tree (3)d
 
----===== Penyusup =====---
-JmlPenyusup = 4 --JUMLAH MINIMAL PENYUSUP AGAR BISA KABUR
-delaykabur = 60000 --WAKTU SAAT BOTNYA KABUR KE EXIT WKWK
-Exit = true --JIKA FALSE, BOTNYA NEXT. JIKA TRUE, BOTNYA DIEM DULU DI 
-
 ---===== EMOJI =====---
 emot_bot = "<:HuTaoPeek:1095236377408450591>"
 emot_gems = "<:gems:1089014830561759345>"
@@ -57,31 +53,17 @@ emot_rank = "<a:kurukuru_seseren:1105870509985509508>"
 emot_tas = "<:agrred:940059592853946388>"
 emot_computer = "<:cpu:994228771014070372>"
 
----===== JOIN RANDOM WORLD =====---
-nextrandom = true --UBAH JADI FALSE JIKA GAMAU JOIN RANDOM WORLD SEBELUM NEXT WORLD
-storagerandom = true --UBAH JADI FALSE JIKA GAMAU JOIN RANDOM WORLD SETELAH BALIK DARI STORAGE
-delayrandomworld = 20000
-
 ---===== COCOK UNTUK EVENT! =====---
 SpecialItem = {5746, 5742, 5748} --ITEM YANG INGIN DIAMANKAN KE WORLD, COCOK UNTUK HARVEST FEST!
 minSpec = 200 --BOT AKAN DROP ITEM APABILA ITEM TSB MELEBIHI TARGET minSpec
 ID_AcuanDrop_Spec = 880 --BOT AKAN DROP SPECIAL ITEM DI DIRT TREE (3)
 
----===== SISTEM DISCONNECT =====---
-removewhendc = false --UBAH JADI FALSE JIKA GA INGIN REMOVE BOT
-batasdc = 5 --BATAS BERAPA KALI SPAM DISCONNECT AGAR DIREMOVE (SELAMA TRY TO RECONNECT DOANG)
-
---=== NEW VARIABLE v7.7 ===--
-ConvertWL = true --JIKA TRUE, SETIAP BOT MEMILIKI 100WL AKAN DIKONVERSI MENJADI DL.
-RemoveSuspend = false --APABILA SALAH SATU BOT LOKAL TERKENA SUSPEND MAKA SEMUA BOT AKAN AUTO REMOVE! BERLAKU APABILA EXENYA SETELAH LOGIN SEMUA BOT
-DelayRecon = 300000 --JEDA RECONNECT
-DelayOffByMod = 60 --MAU RELOGIN BERAPA MENIT SETELAH ADA MODS JOIN?
-
 --=== NEW VAR ===--
-IntervalPlus = 15 -- JEDA TIAP CALL FUNCTION
+IntervalPlus = 10 -- JEDA TIAP CALL FUNCTION
 ControlViaDC = true
-TimeRelogBotMatung = 20 --JIKA TIDAK ADA AKTIVITAS SELAMA 7 MENIT, AUTO RELOG
+TimeRelogBotMatung = 10 --JIKA TIDAK ADA AKTIVITAS SELAMA 7 MENIT, AUTO RELOG
 HideWebhook = false --BOT GAK BAKAL NGIRIM WEBHOOK (CONDITIONAL VARIABLE LANJUTAN)
 ShowPingHook = true --bakal tetap ngasih info untuk PingHook
 ShowSeedInfo = true --bakal tetap ngasih info untuk Seed Hook
 ShowPackInfo = true --bakal tetap ngasih info untuk Pack Hook
+SimplePinghook = true
