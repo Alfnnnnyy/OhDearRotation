c714a542495eb38d5c0ef3d719ffada53c1d0a48
Alfnnnnyy <159158160+Alfnnnnyy@users.noreply.github.com>
2024-03-07 04:48:47 +0700

    Update ConfigRotat.lua

--- ConfigRotat.lua
+++ ConfigRotat.lua
@@ -57,12 +57,12 @@ targettrash = 100 -- AUTO TRASH JUNK ITEMS KETIKA MENCUKUPI TARGET
 Turu = true --JADIIN FALSE JIKA GAMAU UNLIMITED WORLD
 
 ---==== PACK SETTINGS ====---
-autobuypack = false
-hargapack = 20000 -- HARGA PACK
-namapack = "world_lock_10_pack" -- NAMA PACK
-MinItemPack = 20 --APABILA JUMLAH ITEM YANG ADA DIDALAM 
-maxbuy = 2 --MAKSIMAL BOTNYA AKAN REPEAT BELI PACK JIKA 
-iditempack = {242}
+autobuypack = true
+hargapack = 1000 -- HARGA PACK
+namapack = "ssp_10_pack" -- NAMA PACK
+MinItemPack = 200 --APABILA JUMLAH ITEM YANG ADA DIDALAM 
+maxbuy = 20 --MAKSIMAL BOTNYA AKAN REPEAT BELI PACK JIKA 
+iditempack = {5706}
 ID_AcuanDrop_Pack = 880 --BOT AKAN DROP PACK DI ACUAN Dirt Tree (3)d
 
 ---===== COCOK UNTUK EVENT! =====---
